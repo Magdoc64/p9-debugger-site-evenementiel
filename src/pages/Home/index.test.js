4299b5c2be2cf62daea78aa58fe5378dc29cb11b
Magdoc64 <magcvdrby@gmail.com>
2025-04-15 10:58:21 +0200

    improved testing

--- src/pages/Home/index.test.js
+++ src/pages/Home/index.test.js
@@ -36,8 +36,8 @@ jest.mock("../../containers/Events", () => (props) => {
 })
 
 const mockPeopleList = jest.fn();
-jest.mock("../../components/PeopleCard", () => (props) =>{
-  mockPeopleList(props);
+jest.mock("../../components/PeopleCard", () => (imageSrc, name, position) =>{
+  mockPeopleList(imageSrc, name, position);
   return <mock-people role="people-list"/>
 })
 
@@ -49,12 +49,18 @@ describe("When a page is created", () => {
   it("a list a people is displayed", () => {
     render (<Home />);
     expect(mockPeopleList).toHaveBeenCalledTimes(6);
+    expect(mockPeopleList.mock.calls[0][0].imageSrc).toBe("/images/stephanie-liverani-Zz5LQe-VSMY-unsplash.png");
+    expect(mockPeopleList.mock.calls[1][0].imageSrc).toBe("/images/linkedin-sales-solutions-pAtA8xe_iVM-unsplash.png");
+    expect(mockPeopleList.mock.calls[2][0].imageSrc).toBe("/images/christina-wocintechchat-com-SJvDxw0azqw-unsplash.png");
+    expect(mockPeopleList.mock.calls[3][0].imageSrc).toBe("/images/jonas-kakaroto-KIPqvvTOC1s-unsplash.png");
+    expect(mockPeopleList.mock.calls[4][0].imageSrc).toBe("/images/amy-hirschi-b3AYk8HKCl0-unsplash1.png");
+    expect(mockPeopleList.mock.calls[5][0].imageSrc).toBe("/images/christina-wocintechchat-com-0Zx1bDv5BNY-unsplash.png");
   })
   it("a footer is displayed", () => {
     render (<Home />);
-    screen.getByTestId("prestation-testid");
-    screen.getByTestId("contact-testid");
-    screen.getByTestId("description-testid");
+    expect(screen.getByTestId("prestation-testid")).toBeInTheDocument();
+    expect(screen.getByTestId("contact-testid")).toBeInTheDocument();
+    expect(screen.getByTestId("description-testid")).toBeInTheDocument();
   })
   it("an event card, with the last event, is displayed", () => {
     // to implement
